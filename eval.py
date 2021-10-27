d3d5cdafd0800d7fd63b0c63d4ac167336493142
canyagmur <can.yagmur@ozu.edu.tr>
2021-10-27 16:05:18 +0300

    Evaluate the model 3 with different type and noise leveled radar signals including Barker, LFM, Rect

--- eval.py
+++ eval.py
@@ -1,9 +1,12 @@
 import cv2
 from keras.backend import reshape
+import os
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 CATEGORIES = ['AM','FM']
-
+PATH = "../datasets/test-set/FM-mixed/"
+MODEL_PATH = "../trained_model/radar-classifier_3.model"
 def prepare(filepath):
     IMG_SIZE = 224
     img_array = cv2.imread(filepath)
@@ -13,11 +16,30 @@ def prepare(filepath):
     #print(reshaped.shape)
     return reshaped
 
-model = tf.keras.models.load_model("radar-classifier.model")
+def data_visualization(img,img_name,label):
+    plt.imshow(img)
+    plt.title('Predictions')
+    plt.xlabel('Predicted Label : '+ label)
+    plt.ylabel('Image Name : '+ img_name )
+    #plt.legend()
+    plt.show()
+
+model = tf.keras.models.load_model(MODEL_PATH)
 
 model.summary()
 
-prediction = model.predict([prepare('AM1.png')])
+testset_files_list = os.listdir(PATH)
+
+for file_name in testset_files_list:
+    abs_path = PATH+file_name
+    img = prepare(abs_path)
+    prediction = model.predict([img])
+    prediction_label = CATEGORIES[int(prediction[0][0])]
+    img = img[0,:,:,:]
+    data_visualization(img,file_name,prediction_label)
 
 
-print("Prediction : "+ CATEGORIES[int(prediction[0][0])])
+
+#print("Prediction : "+ CATEGORIES[int(prediction[0][0])])
+
+
